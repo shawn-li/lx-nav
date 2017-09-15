0f2016c2713ec908ad29c561d072e34de5eefc18
shawn li <707361845@qq.com>
2017-09-15 10:20:14 +0800

    Update 2017-9-15

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 $(document).ready(function(){
 
 	//lx-nav插件
-	$('.lx-nav').lxnav({
+	$(document).lxnav({
 		navWidth:'90%'
+	});
+
+	//lx-slider插件
+	$('#slider').lxslider({
+		boxHeight:500,
+		imgRatio:(5/7),
+		pre_button:'.pre_button',
+		next_button:'.next_button',
+		stop_button:'.stop_button',
+		changeType:'animate',
+		animateTime:1000,
+		autoFlag:false,
+		changeEnd:function(){
+			console.log("yoyoyo");
+		}
 	});
 
 });
